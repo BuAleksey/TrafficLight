0cc35691fd2a41edaca10d6c9999838ed6371ac0
Aleksey <oldf03@gmail.com>
2022-09-20 22:38:54 +0300

    use switch and dynamic size views

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -13,45 +13,53 @@ class ViewController: UIViewController {
     @IBOutlet var yellowSignalLabel: UIView!
     @IBOutlet var greenSignalLabel: UIView!
     @IBOutlet var startButton: UIButton!
-    var taps = 0
+    
+    var currentSignal = Signals.red
+    let signalOff: CGFloat = 0.3
+    let signalOn: CGFloat = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redSignalLabel.layer.cornerRadius = 64
-        redSignalLabel.alpha = 0.3
-        
-        yellowSignalLabel.layer.cornerRadius = 64
-        yellowSignalLabel.alpha = 0.3
-        
-        greenSignalLabel.layer.cornerRadius = 64
-        greenSignalLabel.alpha = 0.3
+       
+        redSignalLabel.alpha = signalOff
+        yellowSignalLabel.alpha = signalOff
+        greenSignalLabel.alpha = signalOff
         
         startButton.layer.cornerRadius = 20
         startButton.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
     }
+    
+    override func viewWillLayoutSubviews() {
+        redSignalLabel.layer.cornerRadius = redSignalLabel.frame.width/2
+        yellowSignalLabel.layer.cornerRadius = yellowSignalLabel.frame.width/2
+        greenSignalLabel.layer.cornerRadius = greenSignalLabel.frame.width/2
+    }
 
     @IBAction func startButtonTaped() {
-        startButton.setTitle("Next", for: .normal)
-        
-        if taps < 3 {
-            taps += 1
-        } else {
-            taps = 1
+        if startButton.titleLabel?.text == "Start" {
+            startButton.setTitle("Next", for: .normal)
         }
         
-        if taps == 1 {
-            redSignalLabel.alpha = 1
-            yellowSignalLabel.alpha = 0.3
-            greenSignalLabel.alpha = 0.3
-        } else if taps == 2 {
-            redSignalLabel.alpha = 0.3
-            yellowSignalLabel.alpha = 1
-            greenSignalLabel.alpha = 0.3
-        } else {
-            redSignalLabel.alpha = 0.3
-            yellowSignalLabel.alpha = 0.3
-            greenSignalLabel.alpha = 1
+        switch currentSignal {
+        case .red:
+            redSignalLabel.alpha = signalOn
+            greenSignalLabel.alpha = signalOff
+            currentSignal = Signals.yellow
+        case .yellow:
+            redSignalLabel.alpha = signalOff
+            yellowSignalLabel.alpha = signalOn
+            currentSignal = Signals.green
+        default:
+            yellowSignalLabel.alpha = signalOff
+            greenSignalLabel.alpha = signalOn
+            currentSignal = Signals.red
         }
+    }
+}
+
+extension ViewController {
+    enum Signals {
+        case red, yellow, green
     }
 }
 
